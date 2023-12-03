afbe3fdf9a3d23fdde70f978f7a352e7832b3e24
Mate Buljan <mate.buljan1@gmail.com>
2023-12-03 15:30:31 +0100

    paths misc

--- Lavender/Core/Paths.cpp
+++ Lavender/Core/Paths.cpp
@@ -5,32 +5,32 @@ namespace lavender
 
 	std::string paths::MainDir()
 	{
-		return LAVENDER_PATH;
+		return LAVENDER_PATH"/";
 	}
 
 	std::string paths::SavedDir()
 	{
-		return MainDir() + "/Saved";
+		return MainDir() + "Saved/";
 	}
 
 	std::string paths::ResourcesDir()
 	{
-		return MainDir() + "/Resources";
+		return MainDir() + "Resources/";
 	}
 
 	std::string paths::FontsDir()
 	{
-		return ResourcesDir() + "/Fonts";
+		return ResourcesDir() + "Fonts/";
 	}
 
 	std::string paths::IconsDir()
 	{
-		return ResourcesDir() + "/Icons";
+		return ResourcesDir() + "Icons/";
 	}
 
 	std::string paths::ScreenshotsDir()
 	{
-		return SavedDir() + "/Screenshots";
+		return SavedDir() + "Screenshots/";
 	}
 
 }
