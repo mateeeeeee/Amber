d9cb17c7077023e238d5e9cb645455f61670a072
Mate Buljan <mate.buljan1@gmail.com>
2024-04-05 20:24:20 +0200

    misc

--- Lavender/Scene/Camera.h
+++ Lavender/Scene/Camera.h
@@ -2,16 +2,61 @@
 
 namespace lavender
 {
-	struct Camera
+	class Camera 
 	{
-		Vector3		position;
-		Quaternion	rotation;
-		float		fov;
+	public:
+		Camera() 
+			: eye(Vector3(1.0f, 1.0f, 1.0f)), lookat(Vector3(0.0f, 0.0f, 0.0f)), up(Vector3(0.0f, 1.0f, 0.0f)), fovy(35.0f), aspect_ratio(1.0f)
+		{
+		}
 
-		float		lens_radius;
-		float		focal_distance;
+		Camera(Vector3 const& eye, Vector3 const& lookat, Vector3 const& up, float fovY, float aspectRatio)
+			: eye(eye), lookat(lookat), up(up), fovy(fovY), aspect_ratio(aspectRatio)
+		{
+		}
 
-		float		shutter_start;
-		float		shutter_end;
+		Vector3 GetDirection() const 
+		{ 
+			Vector3 dir = lookat - eye;
+			Vector3 normalized_dir;
+			dir.Normalize(normalized_dir);
+			return normalized_dir;
+		}
+		void setDirection(Vector3 const& dir) 
+		{ 
+			lookat = eye + dir * dir.Length();
+		}
+
+		Vector3 const& GetEye() const { return eye; }
+		void SetEye(Vector3 const& val) { eye = val; }
+
+		Vector3 const& GetLookat() const { return lookat; }
+		void SetLookat(Vector3 const& val) { lookat = val; }
+
+		Vector3 const& GetUp() const { return up; }
+		void SetUp(Vector3 const& val) { up = val; }
+		float GetFovY() const { return fovy; }
+		void SetFovY(float val) { fovy = val; }
+		float GetAspectRatio() const { return aspect_ratio; }
+		void  SetAspectRatio(float val) { aspect_ratio = val; }
+
+		void GetFrame(Vector3& U, Vector3& V, Vector3& W) const
+		{
+			W = lookat - eye;
+			float wlen = W.Length();
+			U = W.Cross(up); U.Normalize();
+			V = U.Cross(W); V.Normalize();
+			float vlen = wlen * tanf(0.5f * fovy * DirectX::XM_PI / 180.0f);
+			V *= vlen;
+			float ulen = vlen * aspect_ratio;
+			U *= ulen;
+		}
+
+	private:
+		Vector3 eye;
+		Vector3 lookat;
+		Vector3 up;
+		float fovy;
+		float aspect_ratio;
 	};
 }
