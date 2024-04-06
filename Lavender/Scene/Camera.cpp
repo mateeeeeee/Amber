958d94144504cea7415a391c1cd7decac679433d
Mate Buljan <mate.buljan1@gmail.com>
2024-04-06 22:57:31 +0200

    moving camera pitch yaw is still weird but fix it later

--- Lavender/Scene/Camera.cpp
+++ Lavender/Scene/Camera.cpp
@@ -13,48 +13,67 @@ namespace lavender
 		if (input.GetKey(KeyCode::ShiftLeft)) speed_factor *= 5.0f;
 		if (input.GetKey(KeyCode::CtrlLeft))  speed_factor *= 0.2f;
 
-		Vector3 u, v, w;
-		GetFrame(u, v, w);
-
 		if (input.GetKey(KeyCode::W))
 		{
-			eye += speed_factor * dt * w;
+			eye += speed_factor * dt * look_dir;
 		}
 		if (input.GetKey(KeyCode::S))
 		{
-			eye -= speed_factor * dt * w;
+			eye -= speed_factor * dt * look_dir;
 		}
 		if (input.GetKey(KeyCode::A))
 		{
-			eye -= speed_factor * dt * u;
+			eye -= speed_factor * dt * right;
 		}
 		if (input.GetKey(KeyCode::D))
 		{
-			eye += speed_factor * dt * u;
+			eye += speed_factor * dt * right;
 		}
 		if (input.GetKey(KeyCode::Q))
 		{
-			eye += speed_factor * dt * v;
+			eye += speed_factor * dt * up;
 		}
 		if (input.GetKey(KeyCode::E))
 		{
-			eye -= speed_factor * dt * v;
+			eye -= speed_factor * dt * up;
 		}
 		if (input.GetKey(KeyCode::MouseRight))
 		{
 			float dx = input.GetMouseDeltaX();
 			float dy = input.GetMouseDeltaY();
-			//Pitch((int64)dy);
-			//Yaw((int64)dx);
+
+			Matrix R = Matrix::CreateFromAxisAngle(right, 0.2f * DirectX::XMConvertToRadians(dy));
+			up = Vector3::TransformNormal(up, R);
+			look_dir = Vector3::TransformNormal(look_dir, R);
+
+			R = Matrix::CreateRotationY(0.2f * DirectX::XMConvertToRadians(dx));
+			right = Vector3::TransformNormal(right, R);
+			up = Vector3::TransformNormal(up, R);
+			look_dir = Vector3::TransformNormal(look_dir, R);
 		}
+
+		UpdateFrame();
 	}
 
 
 	void Camera::GetFrame(Vector3& U, Vector3& V, Vector3& W) const
 	{
+		U = right;
+		V = up;
 		W = look_dir;
-		U = W.Cross(up); U.Normalize();
-		V = U.Cross(W); V.Normalize();
+
+		float vlen = tanf(0.5f * fovy * DirectX::XM_PI / 180.0f);
+		V *= vlen;
+		float ulen = vlen * aspect_ratio;
+		U *= ulen;
+	}
+
+	void Camera::UpdateFrame()
+	{
+		look_dir.Normalize();
+		up = look_dir.Cross(right);
+		up.Normalize();
+		right = up.Cross(look_dir);
 	}
 
 }
--- Lavender/Scene/Camera.h
+++ Lavender/Scene/Camera.h
@@ -6,14 +6,10 @@ namespace lavender
 	{
 		static constexpr float speed = 0.1f;
 	public:
-		Camera() : eye(Vector3(0.0f, 0.0f, 1.0f)), look_dir(Vector3(0.0f, 0.0f, -1.0f)), up(Vector3(0.0f, 1.0f, 0.0f)), fovy(35.0f), aspect_ratio(1.0f)
+		Camera()  : eye(Vector3(0.0f, 0.0f, 1.0f)), look_dir(Vector3(0.0f, 0.0f, -1.0f))
+			, up(Vector3(0.0f, 1.0f, 0.0f)), right(Vector3(1.0f, 0.0f, 0.0f))
+			, fovy(35.0f), aspect_ratio(1.0f)
 		{
-		}
-
-		Camera(Vector3 const& eye, Vector3 const& lookat, Vector3 const& up, float fovY, float aspectRatio)
-			: eye(eye), up(up), fovy(fovY), aspect_ratio(aspectRatio)
-		{
-			SetLookat(lookat);
 		}
 
 		Vector3 const& GetEye() const { return eye; }
@@ -49,7 +45,11 @@ namespace lavender
 		Vector3 eye;
 		Vector3 look_dir;
 		Vector3 up;
+		Vector3 right;
 		float fovy;
 		float aspect_ratio;
+
+	private:
+		void UpdateFrame();
 	};
 }
