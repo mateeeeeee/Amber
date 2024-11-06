58b5af931ec2dc3cd91562fa9ffcdc1fa30b34a2
Mate Buljan <mate.buljan1@gmail.com>
2024-11-06 08:22:03 +0100

    macros misc

--- Amber/Core/Macros.h
+++ Amber/Core/Macros.h
@@ -7,38 +7,49 @@
 #define AMBER_STRINGIFY(a) _AMBER_STRINGIFY_IMPL(a)
 #define AMBER_CONCAT(x, y) _AMBER_CONCAT_IMPL( x, y )
 
-#define AMBER_ASSERT(expr)			    assert(expr)
-#define AMBER_ASSERT_MSG(expr, msg)     assert(expr && msg)
-
+#define AMBER_ASSERT(expr)			assert(expr)
+#define AMBER_ASSERT_MSG(expr, msg)   assert(expr && msg)
+#define AMBER_UNREACHABLE()			__assume(false)
+#define AMBER_FORCEINLINE			    __forceinline
 #define AMBER_NODISCARD				[[nodiscard]]
 #define AMBER_NORETURN				[[noreturn]]
-#define AMBER_DEPRECATED			[[deprecated]]
-#define AMBER_MAYBE_UNUSED          [[maybe_unused]]
-#define AMBER_DEPRECATED_MSG(msg)	[[deprecated(#msg)]]
-#define AMBER_DEBUGZONE_BEGIN       __pragma(optimize("", off))
-#define AMBER_DEBUGZONE_END         __pragma(optimize("", on))
+#define AMBER_DEPRECATED			    [[deprecated]]
+#define AMBER_MAYBE_UNUSED            [[maybe_unused]]
+#define AMBER_DEPRECATED_MSG(msg)	    [[deprecated(#msg)]]
+#define AMBER_DEBUGZONE_BEGIN         __pragma(optimize("", off))
+#define AMBER_DEBUGZONE_END           __pragma(optimize("", on))
 
+#if defined(_MSC_VER)
+#define AMBER_UNREACHABLE()			__assume(false)
+#define AMBER_DEBUGBREAK()			__debugbreak()
+#elif defined(__GNUC__)
+#define AMBER_UNREACHABLE()			__builtin_unreachable()
+#define AMBER_DEBUGBREAK()	        __builtin_trap()
+#else 
+#define AMBER_UNREACHABLE()			
+#define AMBER_DEBUGBREAK()	        
+#endif
 
-#define AMBER_NONCOPYABLE(ClassName)                 \
-        ClassName(ClassName const&)            = delete; \
-        ClassName& operator=(ClassName const&) = delete;
+#define AMBER_NONCOPYABLE(Class)                 \
+        Class(Class const&)            = delete; \
+        Class& operator=(Class const&) = delete;
 
-#define AMBER_NONMOVABLE(ClassName)                      \
-        ClassName(ClassName&&) noexcept            = delete; \
-        ClassName& operator=(ClassName&&) noexcept = delete;
+#define AMBER_NONMOVABLE(Class)                      \
+        Class(Class&&) noexcept            = delete; \
+        Class& operator=(Class&&) noexcept = delete;
 
-#define AMBER_NONCOPYABLE_NONMOVABLE(ClassName) \
-        AMBER_NONCOPYABLE(ClassName)                \
-        AMBER_NONMOVABLE(ClassName)
+#define AMBER_NONCOPYABLE_NONMOVABLE(Class) \
+        AMBER_NONCOPYABLE(Class)                \
+        AMBER_NONMOVABLE(Class)
 
-#define AMBER_DEFAULT_COPYABLE(ClassName)             \
-        ClassName(ClassName const&)            = default; \
-        ClassName& operator=(ClassName const&) = default;
+#define AMBER_DEFAULT_COPYABLE(Class)             \
+        Class(Class const&)            = default; \
+        Class& operator=(Class const&) = default;
 
-#define AMBER_DEFAULT_MOVABLE(ClassName)                  \
-        ClassName(ClassName&&) noexcept            = default; \
-        ClassName& operator=(ClassName&&) noexcept = default;
+#define AMBER_DEFAULT_MOVABLE(Class)                  \
+        Class(Class&&) noexcept            = default; \
+        Class& operator=(Class&&) noexcept = default;
 
-#define AMBER_DEFAULT_COPYABLE_MOVABLE(ClassName) \
-        AMBER_DEFAULT_COPYABLE(ClassName)             \
-        AMBER_DEFAULT_MOVABLE(ClassName)
+#define AMBER_DEFAULT_COPYABLE_MOVABLE(Class) \
+        AMBER_DEFAULT_COPYABLE(Class)             \
+        AMBER_DEFAULT_MOVABLE(Class)
